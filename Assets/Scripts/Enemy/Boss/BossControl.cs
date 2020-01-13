01f28e2aa86494f7bd965a0272b04dcde8b9d652
xindu233 <32609399+xindu233@users.noreply.github.com>
2020-01-13 11:53:14 +0800

    Boss的基础功能

---
+++ Assets/Scripts/Enemy/Boss/BossControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossControl : EnemyControl
+{
+    private GameObject BossBlood;
+    private void Start()
+    {
+        BossBlood = GameObject.Find("BossBlood");
+    }
+    public override void Hitted(float hp)
+    {
+        Debug.Log("hhh");
+        HP -= hp;
+        BossBlood.GetComponent<Slider>().value = Mathf.Clamp01(HP / maxHP);
+        if (HP <= 0) RecycleNow();
+    }
+}
