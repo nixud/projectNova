d3f07026828e05075cccabc7719d783d798ba0f8
YuXiangguo <xy_guo@foxmail.com>
2020-07-31 00:07:46 +0800

    feat: 添加点击关卡显示不同信息

--- Assets/Scripts/Stage/TouchScriptTest.cs
+++ Assets/Scripts/Stage/TouchScriptTest.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class TouchScriptTest : MonoBehaviour
 {
-    public Text enemyDesc;
+    public Printer enemyDesc;
     
     public StageSceneNew stageSceneNew;
     public float range = 0.3f;
@@ -29,33 +29,35 @@ public class TouchScriptTest : MonoBehaviour
             {
                 if (Vector2.Distance(stageSceneNew.PlayerPlane.transform.position, gameObject.transform.position) <
                     stageSceneNew.Range)
+                {
                     stageSceneNew.StagePointPressed(gameObject);
+                    
+                    StageDiffculty diff;
+                    try
+                    {
+                        diff = gameObject.GetComponent<StageDiffculty>();
+                        Debug.Log(diff.Diffculty);
+                        switch (diff.Diffculty)
+                        {
+                            case "Easy":
+                                enemyDesc.Print("描仪显示该星域内存在小股帝国守军");
+                                break;
+                            case "Medium":
+                                enemyDesc.Print("描仪显示该星域内存在大股帝国守军");
+                                break;
+                            case "Hard":
+                                enemyDesc.Print("扫描仪显示该星域内存在帝国要塞");
+                                break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        enemyDesc.Print("扫描仪显示该星域内存在帝国星系中枢");
+                    }
+                }
             }
 
-            enemyDesc.text = "描仪显示该星域内存在帝国守军";
 
-            // StageDiffculty diff;
-            // try
-            // {
-            //     diff = gameObject.GetComponent<StageDiffculty>();
-            //     Debug.Log(diff.Diffculty);
-            //     switch (diff.Diffculty)
-            //     {
-            //         case "Easy":
-            //             enemyDesc.text = "扫描仪显示该星域内存在小股帝国守军";
-            //             break;
-            //         case "Medium":
-            //             enemyDesc.text = "扫描仪显示该星域内存在大股帝国守军";
-            //             break;
-            //         case "Hard":
-            //             enemyDesc.text = "扫描仪显示该星域内存在帝国要塞";
-            //             break;
-            //     }
-            // }
-            // catch (Exception e)
-            // {
-            //     enemyDesc.text = "扫描仪显示该星域内存在帝国星系中枢";
-            // }
         }
     }
 
---
+++ Assets/Scripts/临时脚本/Printer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Printer : MonoBehaviour
+{
+    public Text Text;
+
+    public void Print(string s)
+    {
+        StartCoroutine(print(s));
+    }
+
+    IEnumerator print(string s)
+    {
+        for (int i = 0; i <= s.Length; i++)
+        {
+            Text.text = s.Substring(0, i);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
